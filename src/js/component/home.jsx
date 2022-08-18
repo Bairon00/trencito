f0fce680ff6c4e0365856f2236e7b949d166ad9b
Bairon00 <bairon97@icloud.com>
2022-08-18 00:33:37 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import { array, number } from "prop-types";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import azul from "../../img/azul.jpg";
+import rojo from "../../img/rojo.png";
+import verde from "../../img/verde.jpg";
+import tren from "../../img/tren.jpg";
+import via from "../../img/via.png";
 //create your first component
 const Home = () => {
+const[Suma,setSuma]=useState([])
+const list =[rojo,azul,verde]
+const randomimg=()=>{return(<img src={list[Suma]}/>)}
+const tareas =(e)=>{e.preventDefault();}
+
+
+
+	
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center ">
+			
+			<div><img className="tamaño2" src={tren}  onClick={() => {
+				if(Suma.length<=12)
+				{setSuma([...Suma, Math.floor(Math.random() * list.length)])}
+				}}/>{Suma.map((e,i)=>{return  <img className="tamaño3" key={i} src={list[e]}/>})}</div>
+			<div><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/><img className="tamaño1" src={via}/></div>
 		</div>
 	);
 };
+
 
 export default Home;
